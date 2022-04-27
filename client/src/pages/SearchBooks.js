05805a5700d16a4af42499e348aa1d55af60245a
dwetoskey <dwetoskey224@gmail.com>
2022-04-26 23:23:19 -0400

    debugging auth

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
+// import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/react-hooks';
 import { SAVE_BOOK, REMOVE_BOOK } from '../utils/mutations';
@@ -32,7 +32,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- server/server.js
+++ server/server.js
@@ -9,18 +9,16 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
   });
-  await server.start();
+
   server.applyMiddleware({ app });
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
 
-startServer()
 
 
 app.use(express.urlencoded({ extended: true }));
